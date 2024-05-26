9408e7f5795c9b9c47cb0dc87cdaaeb0da822d0e
kaloa2025 <aalokchoudhari2021@gmail.com>
2024-05-27 00:16:14 +0530

    Edit Profile

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import profile from '../assests/Profile1.jpg'
 
 function EditForm() {
   return (
     <div>
-      hi
+      <div className='photoSection'>
+        <img src={profile} className='profilePhoto'/>
+        <div>
+            <p className='Nheading'>Name</p>
+            <p className='subtext'>Change profile photo</p>
+        </div>
+      </div>
+      <p className='heading'>Username</p>
+      <input className='input'/>
+      <p className='heading'>Email</p>
+      <input className='input'/>
+      <p className='heading'>Bio</p>
+      <input className='text-input'/>
+      <br/>
+      <button className='saveButton'>Save Changes</button>
     </div>
   )
 }
