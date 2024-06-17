cc7bfa9b38b1b58673ad7e47f8d39cb0f7a893a5
Sarthak Chaure <sarchaure911@gmail.com>
2024-06-17 18:46:08 +0530

    port change

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use((err,req,res,next)=>{
     });
 });
 //Server
-const PORT =process.env.PORT || 3000;
+const PORT =process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`App running on ${PORT}`);
 });
